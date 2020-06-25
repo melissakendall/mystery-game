593557097ecc25be21e1a28889857f4ac8bf86cb
Melissa Kendall <melissak@iqmetrix.com>
2020-06-25 11:22:31 -0600

    Initial commit from bitbucket

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# mystery-game
+# Mystery Game
+
+A Sherlock Holmes style game based on the board game 221b Baker Street: Master Detective.
+
+The stories, clues and solutions are stored in firebase, although the current version of the application hard-codes the data while I transition from Realtime to Firestore.
+
+This repo was originally created in 2017 in Bit Bucket.
---
+++ public/js/script.js
@@ -0,0 +1,134 @@
+var caseDesc = [
+	"A tall, well-dressed woman sits in the parlor of 221 B Baker Street. She wrings an intricately embroidered hankercheif in her hands, betraying her otherwise calm demeanor. \"I beg of you to help me,\" she states in a serious tone.  Watson pours brandy into a crystal glass on the mantle. \"Mrs Forrest, if you would please start at the beginning\". The woman accepts a glass and downs the amber liquid in a single shot, crinkling her nose in distaste. \"You must understand, my Jeremy simply does not have the constitution for murder. Not to mention, he was not acquianted with this woman doctor, this May Quinna\". Watson takes a seat by the fire. \"Why was he staying at the Hotel Grand?\" \"I cannot say, he was supposed to be in Johannesberg visiting his sick mother.\" A knock on the door, and Inspector Lestrade enters the room. \"You asked to see me?\" Watson rises to greet the detective, \"Yes, inspector. I hear you have arrested Jeremy Forrest for the murder of Doctor May Quinna?\" Lestrade huffs, \"Why yes, an open and shut case. The doctor found dead hours before her speech at the conference. A witness reports seeing Mr Forrest leaving the hotel room of the deceased 10 minutes before housekeeping discovered the body. All we have left is to discover how he killed her.\" Suddenly, Sherlock Holmes jumps up, startling the other occupants of the room. \"I am afraid sir, you have arrested an innocent man. Please tell me you interviewed others.\" Lestrade sputters for a minute before recovering his composure. \"Lionel Linquin, the bellboy, delivered room service to Mr. Forrest. Then there was Shana Shen, the housekeeper, who found the doctors body. Oh yes, and Damien Folmett, the guest in the ajoining room.\" Prove Jeremy\'s innocence and discover a) the murderer b) the motive and c) the murder weapon.",
+	"Watson and Holmes are enjoying an afternoon reading when a frantic woman bursts through the door, followed closely by a cursing Inspector Lestrade. \"YOU MUST FIND HER!\" The woman sobs, as Lestrade attempts to restrain her. \"Mrs. Partridge! I assure you, Scotland Yard has the best officers on the case. We will find your daughter.\" \"SHE COULD BE DEAD! SHE COULD BE TAKEN BY...\" The woman crumples to the floor mid-scream. \"She seems to have fainted, Holmes,\" Lestrade says, lifting Mrs. Partridge from the floor into a settee. \"Can\'t say I am much surprised.\" Watson remarks. Lestrade pulls out his notes, \"Lisa Partridge, age 6, disappeared from her school group this morning. Eva Manceo, teacher, contacted the girl's mother and the police shortly after 10am. Also escorting the group, Miss Theresa Pernnet and Miss Juliana Goodwin, both teachers at London's Finishing School for Young Ladies. All other students are accounted for. While the child may have wandered off, we are not discounting the possibility of an abduction. Naturally, we've organized searches of the area they were to be visiting - World Aquarium and Buckingham Palace as well as the surrounding buildings and nearby market\" Pacing the room, Holmes gestures wildy, \"Yes, yes, but have you recovered the cat?\" Lestrade stares at Holmes blankly. \"What cat?\" \"The cat owned by the family\" Holmes gestures to the fainted woman. Watson shakes his head and says, \"There was no mention of a cat, Sherlock.\" Holmes stands in frustration \"Of course there was a cat! The white fur on this woman's dress, the long parallel scratches on her hands. This family had a cat. Find the cat, find the child!\" Discover a) the location of Lisa and b) the motive behind her disappearance. ",
+	"Sherlock Holmes enters the study of Harrison Smyth, barrister. The man in question lays face-down on the floor surrounded by a pool of blood. Paperwork is scattered across the room. As the residents of the room watch in horror, a breeze from the open window shifts a pile of papers onto the floor. A large desk holds writing implements and a shaving kit. \"Bashed in the head\". Watson says, as he closes the window firmly. \"I need not be a surgeon to see the large dent in his temple.\" Inspector Lestrade rises from his position by the body as two new arrivals enter the room. \"Gentlemen, allow me to introduce - Mary Hamel and Jamir Jones, house keeper and gardener for Mr. Smyth\". Jones moves forward, \"I cannot believe anyone would want to kill Harry. I have been part of this household for 25 years. Harry treated us right, he did. He didn\'t deserve to die,\" He moves to position himself in front of Mary as the latter blanches at the sight of blood. \"Aye,\" Mary croaks, \"He was alone all day, I found \'em loike this about four when \'e didn\'t run for coffee. Always rings for coffee did \'arry\". Holmes surveys the room intently, \"Is anything missing?\" Both heads shake. Jones explains, \"He kept the door locked most days\". Mary adds, \"The last few weeks he wouldn\'a go out much\". Lestrade ushers the two out of the room and closes the door. \"Harrison Smythe was one half of Smythe & Baryl, the other being Mortimer Baryl. Without a doubt, Baryl is the killer\". Lestrade holds up a photo of a middle aged bearded man, Smythe, and a tall pale man, Baryl. Holmes looks unconvinced. Discover a) the murderer b) the murder weapon and c) the motive.",
+	"CONFIDENTIAL - POLICE FILE\nCASE No. 155215\nSUBJECT: MULIPLE HOMICIDE\nINQUIRY: a) Discover the identity of the murderer and b) the motive\nNOTE: This case is not scored on number of clues used.",
+	"\"Sherlock Holmes, I humbly ask for your help. The crown princess of Croatia, Zia, has become rebellious – escaping her guards on a regular basis. I fear a diplomatic incident if this behavior does not cease. Please attend to the Hotel Grand tomorrow and ensure her safe passage to Denmark in time for the wedding.\" Sherlock Holmes folds the letter and puts it his pocket, looking around the room disinterested. Watson coughs and steps forward, \"...And that, Princess, is why we are here.\" Across the room, a striking black-haired woman watches as a maid applies dark red varnish to long, maniucured nailes. \"Yes, the council is smothering as always.\". The maid startles and drops the bottle, spilling dark red varnish across the floor. She gasps in horror, \"Mon dieu!-\" Zia cuts off the woman with a wave of her hand, \"Maria, stop. Get up and clean that mess.\" Watson offers a hand to the young woman who graciously accepts, rising to her feet before racing out of the room. \"Well, if you must be here until the carriage arrives, I will introduce my courtiers, Jim and John. I know not their real names.\" The Princess gestures to the men standing near her. Jim, a tall blond man of slight frame, nods. Younger and lanky, he walks with a slight limp. \"Congratuations on your upcoming marriage,\" Watson remarks. Zia examines a large diamond ring on her finger with obvious pleasure. \"Yes. Soon I will be not only Princess of Croatia but also of Denmark\". Sherlock draws Watson aside, \"A scheme is afoot here, Watson.\" What does Sherlock mean? Discover a) what the Princess hiding?",
+	"ARSONIST ARRESTED - June 15th, 1898\nScotland Yard announced an arrest in the case of the Whitechapel Arsonist yesterday. Detective Inspector Gregory Lestrade was caught at the scene of an abandoned Whitechapel boarding house holding a bag filled with paraffin-soaked rags. Police Constable Murcher declined to comment, saying only \"The investigation is ongoing\". Sources close to Scotland Yard cite that Sherlock Holmes and his ever-present companion John Watson are involved in the investigation.\n\nInspector Lestrade\'s eyes are bloodshot, his face unshaven and grim. \"I cannot speak of it,\" he croaks. \"Well, tell us what you can, then,\"  Watson says. The prisoners's eyes dart back and forth for a few seconds, but he remains silent. Sherlock looks pointedly at Watson, \"As I said, if he truly is commited to confessing, why intervene?\" the sleuth stands and shakes the dust from his coat. \"Come now Watson, let us see to clients who WANT our help\".\n\nDiscover a) Why did Lestrade confess? b) Who is the arsonist? and c) Why did the arsonist set the fires?"
+];
+
+var caseSols = [
+	"Lionel Linquin, the bellboy, recognized Dr Quinna when he carried her suitcases to her hotel room. Lionel Linquin\'s father died during trials of Quinna\'s experimental ebola protocol. Seeing an opportunity for revenge, he poisioned her room service with cynanide. Jeremy Forrest lied to his wife and tracked down the doctor to beg for her help treating his mother, who contracted ebola. The arrest was a result of unfortunate timing. ",
+	"Eva Manceo, the biological mother of Lisa, stole the Partridge cat and used the animal to lure her daughter to the aquarium, knowing the hungry cat would follow the scent of fish.",
+	"The body found in the study of Harrison Smythe was, in fact, the body of Smythe's cousin Oliver Clark. Clark - a criminal - immigrated from the continent and stole the identity of his cousin, whom he killed and buried in the garden. Clark was beardless and needed to hide from his servants for a few weeks while he grew a beard to more closely resemble his cousin. Jamir Jones, deeply loyal to Smyth, discovered the body in the garden and killed Clark in revenge.",
+	"Theresa Bali and Percy Jones cooperated to commit the murders. When the children, members of the Silvers gang, began to demand a share of the criminal profits, Theresa asked Percy to dispose of them. Percy decided to incriminated Jonathan Smart in the murders by leaving Jonathan's fillet knife at the scene of the crime and having Theresa's husband report a witness statement. Percy knew Jonathan was angry at being the victim of the pick-pocket gang and counted on Jonathan to further incriminate himself. The proximity of the bodies to the church was coincidence.",
+	"Princess Zia was unhappy with her arranged marriage and switched places with her maid, Maria. The Princess fired her guards so no one would suspect the deception and allow Maria to marry the Prince in her place. While exploring the city enjoying her new freedom, Zia fell in love with a criminal named The Badger. Knowing he was wanted by the police, she was attempting to smuggle the theif into Croatia in the Royal Luggage, which could not be inspected.",
+	"Gregory Lestrade confessed because he believed he was guilty. The real arsonist, Calahan O\'Donnel, wanted revenge on Lestrade for sending his brother to prison. Calahan bribed a maid to drug Lestrade's meals, causing the Inspector to have blackouts. Then it was simply a matter of placing Lestrade near the scene of the crime with incriminating evidence on his person. Lestrade put together his frequent blackouts and current situation and assumed he had committed all of the crimes."
+];
+
+var clues = [
+	["May Quinna died shortly after eating the room service she ordered","Damien Folmett is not the murderer","May Quinna\'s body smelled of bitter almonds", "KILLER CLUE (3 Parts) II - To cover the inside or surface of a garment with a different layer of material", "May Quinna was to present an experimental protocol for the treatment of Ebola","Jeremy Folmett\'s mother contracted ebola","Lionel Linquin\'s father died of Ebola 10 years ago","KILLER CLUE (3 Parts) III - Elizabeth II","KILLER CLUE (3 Parts) I - Zoo animal"],
+	["Mrs. Partridge hates cats","Location (4 Parts) I - To carry around and offer goods for sale, typically by shouting","The Partridge family cat was stolen three days ago", "A cabby reports seeing a hungry-looking white cat lurking near the tour group", "Lisa is not the biological daughter of Mr. and Mrs. Partridge", "Location (4 Parts) IV - Fills gaps in speech when nervous", "Juliana Goodwin reported to Scotland Yard that Eva Manceo  unusually attached to Lisa Partridge", "A hungry cat will follow the smell of fish", "Location (4 Parts) - II - Who, What, When, Why, ____","Location (4 Parts) III - ____ of the beholder","Eva Manceo gave up a baby for adoption"],
+	["Jamir Jones was feircely loyal to his employer","The hotel lists a \"O. Clark\" as a guest","Jamir Jones spent the week before the murder digging up the garden","Motive (3 Parts) I - Me, Myself and ____","It was common for Smyth to leave the windows of the study open while working","Smyth had not left his house for weeks, and would not let his employees see him","The body found in the room was not Harrison Smyth","Motive (3 Parts) II - An indentation","Smythe has a cousin in Spain who is a notorious criminal, known for burying the bodies of his victims","Oliver Clark, Smyth\'s cousin, had recently immigrated from Spain. The two men were similar in apperance but Oliver did not have a beard","Jamir Jones has a criminal history, which his employer overlooked when he hired him 25 years ago. Jones promised Smyth a lifetime of loyalty","Motive (3 Parts) III - The action of stealing","A bloody paper weight was found a few blocks from the scene of the crime"],
+	[ "NEWSPAPER CLIPPING - June 9th, 1888\n\nEVIL STALKS THE STREETS OF LONDON! Orphans and street urchins have gone missing one by one, and Scotland Yard can no longer deny the truth – a serial killer is on the loose. The victims - Abigail Firth, Jamie Grant and Tim McCarthy - each found within blocks of Westhall Church. Children of London fear the dark and the horror each night brings.", "SUSPECTS\n\nJonathan Smart - Fisherman.\nJames Young - Pastor, Westhall Church.\nPercy Jones - Thug.\nTheresa Bali - Proprietor, The King\'s Arms.", "AUTOPSY REPORT\n\nFIRTH, Abigail – Female, Age 10. Cause of death: Exsanguination.\nGRANT, Jamie – Female, Age 12. Cause of death: Exsanguination.\nMCCARTHY, Timothy – Male, Age 8. Cause of death: Exsanguination.\n\nNOTES: No evidence of defensive wounds. The fatal injury in each victim was a severed jugular caused by a thin non-seraded knife approximatly 5-6\" in length.", "ARREST RECORD\n\nPERPETRATOR: Theresa Bali\nDATE: November 5, 1887\nCRIME: Larceny\nSENTENCE: 1 month\nNOTES: Rumored to be ring-leader of Silvers, local gang of pick-pockets.", "ARREST RECORD\n\nPERPETRATOR: Percy Jones\nDATE: May 2, 1888\nCRIME: Trafficing\nSENTENCE: 2 weeks\nNOTES: M.O. is to offer drugged food, once incapacitated vicitims are sent to factory.", "ARREST RECORD\n\nPERPETRATOR: Jonathan Smart\nDATE: June 8, 1888\nCRIME: Public Intoxication\nSENTENCE: 1 day\nNotes: Arrested outside Westhall church, screaming obscenities.", "EVIDENCE\n\nA bloody fillet knife was found near the body of Tim McCarthy.", "WITNESS STATEMENT\n\nName: Clara Rose\nStatement: Witness states the three victims were known to each other.", "WITNESS STATEMENT\n\nName: Rodger Bali\nStatement: Witness saw Jon Smart argued with Kevin McCarthy - father of Tim - the morning before Tim\'s murder. The witness reports hearing Jon say, \"You and that theif son of yours are dead!\"", "WITNESS STATEMENT\n\nName: Hanna Kyle\nStatement: Witness reports James Young had recently started a late-night program to feed street urchins at Westhall church." ],
+	[   "The upcoming marriage of the Princess of Croatia and the Prince of Denmark was pre-arranged", "A recent theft of a priceless marble statue occurred on Thursday night", "No one is permitted to inspect the Royal Luggage", "Lestrade warns Sherlock that a theif named The Badger is terrorizing London and may be attracted to the fortune in jewels the Princess wears", "Zia recently fired her guards and hired replacements, Jim and John.", "When questioned, the guards stated Maria returned late last night from a trip to a beauty salon struggling to carry many large bags.", "Scotland Yard reported the chase of a suspect last night. The chase was aborted when the suspect jumped off a three-story building to escape pursuit.", "Prior to visiting London, the Princess had expressed to the members of the council that she vehemently refused to marry the Prince of Denmark.", "Zia spent most of her childhood in a boarding school in France" ],
+	[ 	"Inspector Lestrade cannot provide an alibi for the time of any of the fires","The arsonist has not caused a single death - each building was unoccupied at the time of the fire.","MOTIVE (3 Parts) II - A queen\'s son.","When he was arrested, Lestrade reportedely repeated the words: \"I don't remember!\" over and over again.","Lestrade has put many criminals behind bars. \nHarry Aliston - Serial Killer\nRyan Vandry - Child abductor\nShawn O\'Donnel - Member of Irish mob","MOTIVE (3 Parts) III - Physically in contact with","Police Constable Murcher recently had a public fight with Lestrade, the two were not on good terms","MOTIVE (3 Parts) I - Oh ____, where art thou?","While searching the Inspector's desk, Sherlock notices a crumpled letter in the garbage: \"You will suffer for what you did to my brother!\". Written on the back in Lestrade's writing, \"Calahan O'Donnel?\"","A maid has come forward and admitted that an Irish man paid her a guinea to sprinkle an unknown white substance in Lestrade's meals the night of the arrest" ]
+];
+
+$( document ).ready(function() {
+
+	//Reset location when case changes and clear input
+	$("#caseNo").change(function() {
+
+		var caseNo = $("#caseNo").val();
+
+		$("#casedescription").val(caseDesc[caseNo]);
+
+		$("#description").val("");
+		$("#solution").val("");
+
+		setClues();
+	});
+
+	//Set clue when clue changes
+	$("#clueList").change(function(){
+
+		var caseNo = $("#caseNo").val();
+		var clueNo = $("#clueList").val();
+
+		if(caseNo > -1 && clueNo > -1) {
+			$("#description").val(clues[caseNo][clueNo]);
+		}
+		else {
+			$("#description").val("");			
+		}
+	});
+
+	//Show solution
+	$("#solve").click(function() {
+
+		var caseNo = $("#caseNo").val();
+
+		if(caseNo > -1) {
+			$("#solution").val(caseSols[caseNo]);
+		}
+	});
+
+	$("#nextClue").click(function() {
+
+		var caseNo = $("#caseNo").val();
+		var clueNo = $("#clueList").val();
+		var nextClue = getNextClue();
+
+		if(nextClue != null) {
+			$("#clueList").val(nextClue);
+			$("#description").val(clues[caseNo][nextClue]);	
+		}
+	});
+	
+	$("#prevClue").click(function() {
+
+		var caseNo = $("#caseNo").val();
+		var clueNo = $("#clueList").val();
+		var prevClue = getPreviousClue();
+
+		if(prevClue != null) {
+			$("#clueList").val(prevClue);
+			$("#description").val(clues[caseNo][prevClue]);	
+		}
+	});
+
+
+	//Set up the clues for the case
+	function setClues() {
+
+		$("#clueList").empty();
+
+		//Not idea, recreates HTML
+		$("#clueList").append( $("<option>", { value: -1, text: "Select a Clue", selected: true }) );
+
+		var caseNo = $("#caseNo").val();
+
+		for(var i = 0; i < clues[caseNo].length; i ++ ) {
+			$('#clueList').append($('<option>', {value: i, text: 'Clue #' + (i + 1) }));
+		}
+	}
+
+	function getNextClue() {
+
+		var caseNo = parseInt($("#caseNo").val());
+		var clueNo = parseInt($("#clueList").val());
+
+		var nextClue = null;
+
+		if(caseNo > -1 && (clues[caseNo].length > clueNo + 1) ) {
+			nextClue = clueNo + 1;
+		}
+
+		return nextClue;
+	}
+
+	function getPreviousClue() {
+		var caseNo = parseInt($("#caseNo").val());
+		var clueNo = parseInt($("#clueList").val());
+
+		var previousClue = null;
+
+		if(caseNo > -1 && clueNo > 0) {
+			previousClue = clueNo - 1;
+		}
+
+		return previousClue;
+
+	}
+});
